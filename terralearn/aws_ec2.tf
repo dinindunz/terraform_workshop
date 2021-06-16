9ea6ab1da890b790b90230a1aa40554a502fa1a6
dinindunz <dinindunz@gmail.com>
2021-06-16 18:56:40 +1200

    1.2.19

--- terralearn/aws_ec2.tf
+++ terralearn/aws_ec2.tf
@@ -28,6 +28,36 @@ resource "aws_instance" "k8_master" {
     }
 }
 
+resource "aws_instance" "k8_slave" {
+    ami = "${lookup(var.AMI, var.AWS_REGION)}"
+    instance_type = "t2.medium"  
+
+    subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+    vpc_security_group_ids = [ "${aws_security_group.ssh.id}", "${aws_security_group.http_https.id}"  ]
+    key_name = var.KEY_NAME
+
+    connection {
+        type        = "ssh"
+        user        = "ubuntu"
+        private_key = "${file(var.PRIVATE_KEY)}"        
+        host        = self.public_ip #"${aws_instance.k8_master.public_ip}"
+    }
+    provisioner "file" {
+        source = "userdata/k8_slave.sh"
+        destination = "/tmp/k8_slave.sh"
+    }
+    provisioner "remote-exec" {
+        inline = [
+            "chmod +x /tmp/k8_slave.sh",
+            "sudo -S /tmp/k8_slave.sh"
+        ]
+    }
+
+    tags = {
+        "name" = "k8_slave"
+    }
+}
+
 #resource "aws_key_pair" "virginia-region-key-pair" {
 #    key_name = var.KEY_NAME
 #    public_key = "${file(var.PUBLIC_KEY)}"
